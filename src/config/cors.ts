a0cd15bb3b05e331cdde34662e91d896372d3299
umerwe <uemyy1@gmail.com>
2026-05-23 17:46:27 +0500

    commented methods

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,5 +4,5 @@ import { config } from "./env";
 export const corsOptions = cors({
   origin: config.clientUrl,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  // methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 });
